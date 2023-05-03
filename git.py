1e2b757c6257d182fca43e004622f70421a02c4d
drishyashrestha <shresthadrishya06@gmail.com>
2023-05-03 09:12:25 +0545

    new

--- git.py
+++ git.py
@@ -1,2 +1,17 @@
 #git init
-git
+#git add .    (git add --all)
+#git commit -m "<commit_message>"
+#git remote add origin <ssh link>
+#git push -u origin <branch_name>
+
+#Setting git configuration
+#git config --global user.name "<username> "
+#git config --global user.email "<email id>"
+#For using another account for a particular project  (git config user.name)
+
+
+
+#generating ssh keys
+#ssh-keygen -t ed25519 -C "your_email@example.com"
+#currntbranch
+#git branch (Shows all branch and highlights the current branch)
